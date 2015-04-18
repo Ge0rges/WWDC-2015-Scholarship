a9818bde27838dbf556b1953fa6dabb7776d0954
Georges Kanaan <ge0rges@ge0rges.com>
2015-04-18 13:12:58 +0300

    Add and complete project view controller layout.  Change target to 8.0.

---
+++ SKInvaders/ProjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectViewController.h
+//  SKInvaders
+//
+//  Created by Georges Kanaan on 4/18/15.
+//  Copyright (c) 2015 RepublicOfApps, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectViewController : UIViewController
+
+@end
---
+++ SKInvaders/ProjectViewController.m
@@ -0,0 +1,37 @@
+//
+//  ProjectViewController.m
+//  SKInvaders
+//
+//  Created by Georges Kanaan on 4/18/15.
+//  Copyright (c) 2015 RepublicOfApps, LLC. All rights reserved.
+//
+
+#import "ProjectViewController.h"
+
+@interface ProjectViewController ()
+
+@end
+
+@implementation ProjectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
